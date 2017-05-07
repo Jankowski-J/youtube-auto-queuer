6a508ed60a912bea98ef21c780e1b0f78327d30f
Jakub Jankowski <jankowski.jakub@outlook.com>
2017-05-07 15:31:18 +0200

    fixed .travis.yml
    trying to fix travis.yml
    
    another approach for testing
    
    - adding unit tests

---
+++ YoutubeQueuerJS/server/scheduling/scheduling-utils.js
@@ -0,0 +1,3 @@
+function createScheduleTimesForHour(hour, schedulesCount) {
+
+}
---
+++ YoutubeQueuerJS/spec/support/dummySpec.js
@@ -0,0 +1,5 @@
+describe("dummy test", function() {
+    it('should be true', function() {
+        expect(1+1).toBe(2);
+    });
+});
